f77f5fd193bcaa258d5124b2fda84f431f760f5e
Oguzhan Ozcan <ouzmetu@gmail.com>
2023-08-31 21:59:46 +0300

    Palette updates
    .cover image updated
    .primary color palette added
    -secondary color palette added
    -semantic color palette added

--- src/shared/presets/index.ts
+++ src/shared/presets/index.ts
@@ -2,11 +2,17 @@ import { HexPalette } from '../types'
 import jdscorepalette from './jdscorepalette.json'
 import jdscorepalette2 from './jdscorepalette2.json'
 import jdscorepalette3 from './jdscorepalette3.json'
+import jdscorepalette4 from './jdscorepalette4.json'
+import jdscorepalette5 from './jdscorepalette5.json'
+import jdscorepalette6 from './jdscorepalette6.json'
 
 
 export const PRESETS: HexPalette[] = [
   jdscorepalette,
   jdscorepalette2,
   jdscorepalette3,
+  jdscorepalette4,
+  jdscorepalette5,
+  jdscorepalette6,
   
 ]
